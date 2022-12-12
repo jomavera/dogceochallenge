5196bb7d2520be32a2de56be4391515595ef2553
jomavera <josemanuel.vera.aray@gmail.com>
2022-12-12 18:12:28 -0300

    Improve filtering of subbreeds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { searchImagesBreed, searchImagesSubBreed } from './api'
-import BarraOpciones from './components/BarraOpciones'
-import Cards from './components/Cards'
+import { searchImagesBreed, searchImagesSubBreed } from './api';
+import BarraOpciones from './components/BarraOpciones';
+import Cards from './components/Cards';
 import Paginacion from './components/Paginacion';
-import 'lodash.product'
-import _ from 'lodash'
+import 'lodash.product';
+import _ from 'lodash';
 
 
 function App() {
@@ -17,28 +17,29 @@ function App() {
 
   useEffect(() => {
     if (imagesState && imagesState.length > 0) {
-      setNumImages(imagesState.length)
-      setImagesSelec(imagesState.slice(0, numPerPage))
+      setNumImages(imagesState.length);
+      setImagesSelec(imagesState.slice(0, numPerPage));
     }
   }, [imagesState]);
 
   const selectSubset = (page) => {
-    setImagesSelec(imagesState.slice((page - 1) * numPerPage, numPerPage * page))
+    setImagesSelec(imagesState.slice((page - 1) * numPerPage, numPerPage * page));
   }
 
   const refreshImages = (e) => {
     if (e[1].length > 0) {
-      let cartesian = _.product(e[0], e[1])
+      let cartesian = _.product(e[0], e[1]);
       let promises = [];
       cartesian.forEach(breed => {
-        promises.push(searchImagesSubBreed(breed[0], breed[1]))
+        promises.push(searchImagesSubBreed(breed[0], breed[1]));
       })
-      const responses = Promise.allSettled(promises)
+      const responses = Promise.allSettled(promises);
       responses.then(resp => {
-        let images = []
+        let images = [];
         resp.forEach(e => {
-          if (e.status === "fulfilled") {
-            e.value.message.forEach(sb => images.push(sb))
+          if (e.status === "fulfilled" && e.value.status !== "error") {
+            console.log(e)
+            e.value.message.forEach(sb => images.push(sb));
           }
         })
         setImages(images)
@@ -46,15 +47,15 @@ function App() {
     } else {
       let promises = [];
       e[0].forEach(breed => {
-        promises.push(searchImagesBreed(breed))
+        promises.push(searchImagesBreed(breed));
       })
-      const responses = Promise.all(promises)
+      const responses = Promise.all(promises);
       responses.then(resp => {
-        let images = []
+        let images = [];
         resp.forEach(e => {
-          e.message.forEach(sb => images.push(sb))
+          e.message.forEach(sb => images.push(sb));
         })
-        setImages(images)
+        setImages(images);
       })
     }
   }
@@ -63,7 +64,7 @@ function App() {
 
   return (
     <div>
-      <h1 className="ui center aligned header mt-3"> Dog CEO Challenge</h1>
+      <h1 className="ui center aligned header mt-3">Dog CEO Challenge</h1>
       <div className="ui container">
         <div className='row'>
           <BarraOpciones onSubmit={(e) => refreshImages(e)} />
